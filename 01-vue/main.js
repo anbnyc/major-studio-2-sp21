565bc361cad021998e294a3aebd28a7fc4452974
EC2 Default User <ec2-user@ip-172-31-19-231.ec2.internal>
2021-01-25 00:13:59 +0000

    init vue/d3 demo

---
+++ 01-vue/main.js
@@ -0,0 +1,38 @@
+/* global Vue, d3 */
+
+const App = {
+  data() {
+    return {
+      bars: [
+          { x: 0, y: 3 },
+          { x: 1, y: 4 },
+          { x: 2, y: 5 }
+        ],
+      inputNumber: 0,
+      width: 300,
+      height: 200,
+    }
+  },
+  methods: {
+      addBar() {
+          const value = this.inputNumber;
+          this.bars.push({ x: this.bars.length, y: value })
+      }
+  },
+  computed: {
+      xScale() {
+          return d3.scaleBand()
+            .paddingInner(0.1)
+            .domain(this.bars.map(d => d.x))
+            .range([0, this.width])
+      },
+      yScale() {
+          return d3.scaleLinear()
+            .domain([0, d3.max(this.bars, d => d.y)])
+            .range([0, this.height])
+            
+      }
+  }
+}
+
+Vue.createApp(App).mount('#app');
---
+++ README.md
@@ -0,0 +1,7 @@
+# Major Studio 2, Spring 2021
+
+### Labs:
+
+#### 01-vue
+_(Evening section, 2021-01-26)_
+A simple SVG/D3 example using Vue
