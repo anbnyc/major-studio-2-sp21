5a0e378bb6b7b612f8b847354250d8851648fdbd
EC2 Default User <ec2-user@ip-172-31-19-231.ec2.internal>
2021-04-08 02:27:38 +0000

    add basic 08-vue-router

---
+++ 08-vue-router/src/components/AboutContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AboutContent',
+  components: {
+
+  },
+  computed: {
+      params(){
+          return this.$route.params
+      }
+  }
+}
+</script>
---
+++ 08-vue-router/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <AboutContent />
+  </div>
+</template>
+
+<script>
+import AboutContent from '@/components/AboutContent.vue'
+
+export default {
+  name: 'About',
+  components: {
+    AboutContent
+  }
+}
+</script>
